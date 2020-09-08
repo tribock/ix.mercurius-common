79001d6f30f9cbf1926cdfb06e90e6e2a107ca78
tribock <louis.baumann@vrsg.ch>
2020-09-08 11:35:22 +0200

    create file first

--- common.go
+++ common.go
@@ -20,14 +20,15 @@ func fileContainsText(textToAppend string, path string) bool {
 	return strings.Contains(string(file), textToAppend)
 }
 func writeLines(textToAppend string, path string) error {
-	if fileContainsText(textToAppend, path) {
-		return nil
-	}
 	// overwrite file if it exists
 	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
+	if fileContainsText(textToAppend, path) {
+		return nil
+	}
+
 	defer file.Close()
 	// new writer w/ default 4096 buffer size
 	w := bufio.NewWriter(file)

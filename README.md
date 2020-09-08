5ff593727f4bfb91a896eb695570eed474fbe622
tribock <louis.baumann@vrsg.ch>
2020-09-08 10:32:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# common functions for ix.mercurius
+
+## init
+
+```bash
+export GO111MODULE=on
+```
---
+++ common.go
@@ -0,0 +1,66 @@
+package common
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func appendToDoku(envParam string) {
+	err := writeLines(envParam, "./env.md")
+	if err != nil {
+		panic(err)
+	}
+}
+func fileContainsText(textToAppend string, file *os.File) bool {
+	scanner := bufio.NewScanner(file)
+	b, err := ioutil.ReadAll(file)
+	FailOnError(err)
+	fmt.Print(b)
+	log.Println(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		log.Println("HODOOOOOR")
+		log.Println(scanner.Text(), textToAppend)
+		if strings.Contains(scanner.Text(), textToAppend) {
+			return true
+		}
+	}
+	return false
+}
+func writeLines(textToAppend string, path string) error {
+	// overwrite file if it exists
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	log.Println(textToAppend)
+	if fileContainsText(textToAppend, file) {
+		return err
+	}
+	// new writer w/ default 4096 buffer size
+	w := bufio.NewWriter(file)
+	_, err = w.WriteString(textToAppend + "\n")
+	if err != nil {
+		return err
+	}
+	// flush outstanding data
+	return w.Flush()
+}
+func Getenv(key, fallback string) string {
+	appendToDoku(key)
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+func FailOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
